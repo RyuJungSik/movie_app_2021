74eacbe15f01d7c011edc32563cf3cbe4c0c83a4
RyuJungSik <xvbc123@gmail.com>
2021-01-01 18:23:39 +0900

    init

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Proptypes from "prop-types";
 import "./Movie.css";
 
 function Movie({year, title, summary, poster, genres }) {
   return (
+    <Link
+    to={{
+      pathname: "movie-detail",
+      state: {
+        year,
+        title,
+        summary,
+        poster,
+        genres
+      }
+     }}
+    >
+
+
   <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
       <h3 className="movie__title">{title}</h3>
       <h5 className="movie__year">{year}</h5>
-      <p className="movie__summary">{summary}</p>
+      <p className="movie__summary">{summary.slice(0, 140)}...</p>
       <ul className="movie__genres">
            {genres.map((genre, index) =>(
           <li key={index} className="genres__genre">{index} {genre}</li>
           ))}
           </ul>
+          
   </div>
   </div>
+  </Link>
   );
 }
 
---
+++ src/routes/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about__container">
+        <sapn>
+            "Freedom is the freedom to say that two plus two maek four. If that i granted
+            all  else follews"
+        </sapn>
+        <span>-GEorge Orwell, 1984 </span>
+    </div>
+  );
+
+
+
+  
+}
+
+export default About;
